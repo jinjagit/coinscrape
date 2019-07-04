aa6d70d9c74c30b0f9ce1c712b2e9642d62377a0
Simon Tharby <jinjamail@gmail.com>
2019-07-04 15:42:15 +0100

    successful scrape of price at specific date on CoinMarketCap

--- hello.rb
+++ hello.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20180703&end=20190703'))
+tds = doc.css('td')
+
+date = "Jul 01, 2019"
+result = ""
+
+tds.length.times do |i|
+  if tds[i].to_s.include? date
+    result = tds[i + 4].to_s.split('">')[1].split('</td>')[0]
+  end
+end
 
 get '/' do
-  "Hello World!"
+  "#{date}: #{result}"
 end
